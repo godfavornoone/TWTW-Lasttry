1e4aff4cb3a35e21c418a540c0f35068fa3c655a
Kittinat Pornsurapab <tsundereneko@gmail.com>
2016-02-07 18:39:18 +0700

    Change Structure Success in Folder Test Structure

---
+++ Test_Structure/Assets/Typing_Input.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Typing_Input : MonoBehaviour {
+
+	[HideInInspector]
+	public string textFieldString;
+
+	private char textFieldChar;
+
+	void Update () {
+
+		if(Input.GetKeyDown(KeyCode.Escape)){
+			Game_Controller.ESC = true;
+		}else if (!Input.GetKeyDown(KeyCode.LeftShift) && !Input.GetKeyDown(KeyCode.Tab) && Input.anyKeyDown) {
+			Game_Controller.ESC = false;
+			textFieldString = Input.inputString;
+			textFieldChar = textFieldString [0];
+			
+			foreach(Enemy enemy in Game_Controller.enemyInThisMap){
+				enemy.CheckWrongAll(textFieldChar);
+			}
+			
+			if(!Game_Controller.wrongAll){
+				foreach(Enemy enemy in Game_Controller.enemyInThisMap){
+					enemy.CheckLetter(textFieldChar);
+				}
+				Game_Controller.indexGlobal++;
+				Game_Controller.wrongAll = true;
+
+			}
+			//if wrongall = true kue mun pid mod we won't do down but if it is false we will do
+			//it should get the result of true or false...if it is true then go to down...
+		}
+	}
+}
---
+++ Test_Structure/Assets/UIManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+
+	public GameObject target;
+	public Transform startPosition;
+
+	string sName;
+
+	void Awake(){
+		DontDestroyOnLoad(transform.gameObject);
+	}
+
+	public void switchScene(string sceneName){
+		Application.LoadLevel(sceneName);
+	}
+
+	void OnLevelWasLoaded(int level)
+	{
+		Debug.Log ("OnLevel work");
+
+		if (level == 1) {
+
+			Debug.Log ("OnLevel work inside");
+
+			Instantiate (target, new Vector3(-3.944f,-1.014f,0f),Quaternion.identity);
+			Game_Controller.playerInThisMap = GameObject.FindGameObjectWithTag ("Player").GetComponent<Player> ();
+			GameObject[] enemySpawn = GameObject.FindGameObjectsWithTag("Enemy");
+			Game_Controller.enemyInThisMap.Clear ();
+			foreach(GameObject enemy in enemySpawn){
+				Game_Controller.enemyInThisMap.Add(enemy.GetComponent<Enemy>());
+			}
+			foreach (GameObject enemy in enemySpawn) {
+				Debug.Log(enemy.name);
+			}
+		}
+	}
+
+
+	public void Quit(){
+		Application.Quit ();
+	}
+}
